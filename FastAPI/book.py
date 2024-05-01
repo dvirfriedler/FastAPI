3260e4b7fd5d8c8012976fd8299ef2c8125af4b3
dvirfriedler <dvir.friedler@gmail.com>
2024-05-01 14:30:44 +0300

    Project2 finished

--- FastAPI/book.py
+++ FastAPI/book.py
@@ -4,11 +4,13 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
     
     
-    def __init__(self, id: int, title: str, author: str, description: str, rating: int):
+    def __init__(self, id: int, title: str, author: str, description: str, rating: int, published_date: int):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
