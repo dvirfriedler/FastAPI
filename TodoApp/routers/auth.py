93adb4bbbbecf4b55292d942bbdddcffa17b4b63
dvirfriedler <dvir.friedler@gmail.com>
2024-05-10 11:27:23 +0300

    Update phone number filed was add to user.py

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -90,7 +90,8 @@ async def create_user(db: db_dependency,
             last_name = create_user_request.last_name,
             role = create_user_request.role,
             hashed_password = bcyrpt_context.hash(create_user_request.password),
-            is_active = True
+            is_active = True,
+            phone_number = create_user_request.phone_number
             )
         
         db.add(create_user_model)
--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -54,3 +54,15 @@ async def change_password(user: user_dependency,
     db.commit()
     return {"message": "Password updated successfully"}
 
+@router.put('/phone_number', status_code=status.HTTP_204_NO_CONTENT)
+async def change_phone_number(user: user_dependency,
+                              db: db_dependency,
+                              phone_number: str = Query(..., min_length=10, max_length=10)):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+    user_model.phone_number = phone_number
+    db.add(user_model)
+    db.commit()
+    return {"message": "Phone number updated successfully"}
+
