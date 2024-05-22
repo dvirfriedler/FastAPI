8b012c0eb62df32a73d5725ef25547a7468f20fa
dvirfriedler <dvir.friedler@gmail.com>
2024-05-22 14:13:07 +0300

    remove necessary code from main.py

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -16,9 +16,6 @@ Base.metadata.create_all(bind=engine)
 def healthcheck():
     return {"status": "healthy"}
 
-static_directory = os.path.join(os.path.dirname(__file__), "static")
-app.mount("/static", StaticFiles(directory=static_directory), name="static")
-
 app.include_router(users.router)
 app.include_router(admin.router)
 app.include_router(auth.router)
